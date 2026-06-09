599dca3e2e69dc4b686f74523a2410700ace704f
Bethelhem Ashenafi <bettyashu5@gmail.com>
2026-06-09 09:35:53 +0300

    update payment option

--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+564cc771176aa9774d0868994865d5dd01d427a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5be014639a544356e2c3dfc19dd6e9c19094b7e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
